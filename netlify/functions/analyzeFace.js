e8ec89416eb110482b3305951fc918d6c7945f44
DGS <emil.pride@gmail.com>
2025-08-29 01:11:14 +0200

    Update analyzeFace.js

--- netlify/functions/analyzeFace.js
+++ netlify/functions/analyzeFace.js
@@ -1,8 +1,7 @@
 const axios = require('axios');
 const querystring = require('querystring');
 
-// Correct API endpoints from the documentation
-const DETECT_URL = 'https://api-us.faceplusplus.com/facepp/v3/detect';
+const FACEPP_DETECT_URL = 'https://api-us.faceplusplus.com/facepp/v3/detect';
 const ANALYZE_URL = 'https://api-us.faceplusplus.com/facepp/v3/face/analyze';
 
 exports.handler = async (event) => {
@@ -12,14 +11,12 @@ exports.handler = async (event) => {
 
   try {
     const { photoDataUrl } = JSON.parse(event.body);
-
     if (!photoDataUrl) {
       return { statusCode: 400, body: 'Photo data is required' };
     }
 
     const base64Image = photoDataUrl.split(';base64,').pop();
 
-    // --- STEP 1: DETECT FACE and GET face_token ---
     const detectFormData = {
       api_key: process.env.FACEPLUSPLUS_API_KEY,
       api_secret: process.env.FACEPLUSPLUS_API_SECRET,
@@ -34,12 +31,11 @@ exports.handler = async (event) => {
 
     const faceToken = detectResponse.data.faces[0].face_token;
 
-    // --- STEP 2: ANALYZE FACE using face_token ---
     const analyzeFormData = {
       api_key: process.env.FACEPLUSPLUS_API_KEY,
       api_secret: process.env.FACEPLUSPLUS_API_SECRET,
       face_tokens: faceToken,
-      // We request the attributes here, using the correct names from the documentation
+      // Corrected attributes list
       return_attributes: 'gender,age,smiling,emotion,glasses'
     };
 
@@ -51,11 +47,10 @@ exports.handler = async (event) => {
 
     const faceAttributes = analyzeResponse.data.faces[0].attributes;
 
-    // Structure the final result object to be compatible with your Gemini prompt
     const analysisResult = {
       age: faceAttributes.age.value,
       gender: faceAttributes.gender.value,
-      smile: faceAttributes.smiling.value > faceAttributes.smiling.threshold,
+      smile: faceAttributes.smiling.value > faceAttributes.smiling.threshold, // Use the correct 'smiling' attribute
       glasses: faceAttributes.glass.value,
       emotion: faceAttributes.emotion
     };
